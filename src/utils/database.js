b943f7d16c8f9b4ae9e7bdcb80623db0d506fdb4
innovativeprogrammingorg <vanish172@gmail.com>
2018-03-24 15:24:00 -0400

    database functions

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,11 +1,13 @@
-import {MongoClient} = from 'mongodb';
+import * as MongoClient = from 'mongodb';
 import {MONGODB_URL} from '../config/constants.js';
+import User from '../server/objects/user.js';
 
 const database = "cah_clone"
 const url = MONGODB_URL+database;
 
 export const collections = [
-	"users"
+	"users",
+	"games"
 ];
 
 export const init = ()=>{
@@ -20,95 +22,55 @@ export const init = ()=>{
 			    	db.close();
 			  	});
 		  	}
-		  	
 		});
 
 	}catch(err){
 
 	}
-};	
-
-function Comment(poster,comment){
-	if(typeof poster != "string" || typeof comment != "string"){
-		throw "InvalidArgumentException";
-	}
-	const uuidv4 = require('uuid/v4');
-	this._id = uuidv4();
-  	this.poster = poster;
-  	this.comment = comment;
-}
-
-function Ingredient(name,amount){
-	this.name = name;
-	this.amount = amount;
-}
-
-const createIngredient = (name,amount)=>{
-	return new Ingredient(name,amount);
-}
-
-const createComment = (poster,comment)=>{
-	return new Comment(poster,comment);
-}
-
-function Recipe(title,ingredients,steps,comments){
-	console.log("Creating recipe object");
-	if(typeof title != "string"){
-		throw new Error("Bad title");
-	}
-	if(!(ingredients instanceof Array) || (ingredients.length > 0 && !(ingredients[0] instanceof Ingredient))){
-		throw new Error("Bad ingredients");
-	}
-	if(!(steps instanceof Array) || (steps.length > 0 && typeof steps[0] != "string")){
-		throw new Error("bad steps");
-	}
-	if(!(comments instanceof Array) || (comments.length > 0 && !(comments[0] instanceof Comment))){
-		throw new Error("bad comments");
-	}
-	const uuidv4 = require('uuid/v4');
-
-	this._id = uuidv4();
-  	this.title = title;
-  	this.ingredients = ingredients;
-  	this.steps = steps;
-  	this.comments = comments;
-  	console.log("Created Recipe object");
-}
+};
 
 
-const createRecipe = (title,ingredients,steps,comments)=>{
+export const getUser = (username)=>{
 	return new Promise((resolve,reject)=>{
-		var recipe;
-		recipe = new Recipe(title,ingredients,steps,comments);
-		 
-		console.log(recipe);
-		console.log("Adding recipe");
-		MongoClient.connect(url, function(err, db) {
-			if (err) throw err;
-			db.collection("recipes").insertOne(recipe, function(error, res) {
-				if (error) throw error;
-			    db.close();
-		  	});
-		});
-		resolve(recipe);
-	
-	});
-}
-
-const getAll = ()=>{
-	return new Promise((resolve,reject)=>{
-		
+		if(typeof id == "undefined"){
+			throw new Error("undefined id");
+		}
 		MongoClient.connect(url, function(err, db) {
 		  	if (err) throw err;
-		  	db.collection("recipes").find({}).toArray(function(error, result) {
-		    	if (error) throw error;
-		    	resolve(result);
-		    	db.close();
-		  	});
+		  	db.collection("users").findOne({ username: username },function(error, result) {
+			    if (error) throw error;
+			    resolve(result);
+			    db.close();
+			});
+		  	
 		});
-		
 	});
-}
+};
+
+export async function userExists(username){
+	try{
+		let results = await getUser(username);
+		return results == null;
+	}catch(err){
+		throw err;
+	}
+	
+}	
+
+export async function insertUser(user){
+	if(userExists(user.username)){
+		throw new Error("User already exists");
+	}
+	await MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		db.collection("users").insertOne(user, function(error, res) {
+			if (error) throw error;
+		    db.close();
+	  	});
+	});
+
+};
+
 const getR = (id)=>{
 	return new Promise((resolve,reject)=>{
 		if(typeof id == "undefined"){
@@ -144,8 +106,6 @@ const update = (id,data)=>{
 	    		getR(id).then(task=>{
 	    			resolve(task);
 	    		});
-		    		
-		    	
 		    	db.close();
 		  	});
 		});
@@ -171,14 +131,3 @@ const remove = (id)=>{
 	});
 }
 
-module.exports = {
-	init,init,
-	getAll:getAll,
-	getR:getR,
-	createRecipe:createRecipe,
-	createIngredient:createIngredient,
-	createComment:createComment,
-	update:update,
-	remove:remove
-
-}
